db07854561731cbbfe5ac8b997738a846a6fd841
plugowski <plugowski@corelogic.pl>
2021-12-01 19:55:59 +0100

    first day, solution and data

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    class Summary(val measurementIncreasesAmount: Int = -1, val previous: Int = 0)
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        val increasedAmount = input.fold(Summary()) { summary, next ->
+            if (next > summary.previous) {
+                Summary(summary.measurementIncreasesAmount + 1, next)
+            } else {
+                Summary(summary.measurementIncreasesAmount, next)
+            }
+        }
+        return increasedAmount.measurementIncreasesAmount
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map(String::toInt)
+    check(part1(testInput) == 11)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map(String::toInt)
     println(part1(input))
-    println(part2(input))
 }
